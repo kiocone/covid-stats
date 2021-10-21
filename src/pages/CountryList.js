1666317cb9ade45bd1e57bf4c72d3ca28287acf5
Kiocone <kiocone@gmail.com>
2021-10-21 10:32:24 -0500

    countryList component Update

--- src/pages/CountryList.js
+++ src/pages/CountryList.js
@@ -11,7 +11,7 @@ class CountryList extends Component {
     async componentDidMount() {
         const res = await axios.get('https://disease.sh/v3/covid-19/countries')
         if (res.status === 200) {
-            console.log(res.data.active)
+            //console.log(res.data.active)
             this.setState({
                 res: res,
                 loading: false
@@ -23,27 +23,31 @@ class CountryList extends Component {
         var allData_html_table = "";
         if (this.state.loading) {
             allData_html_table =
-                <tbody>
-                    <tr>
-                        <td colSpan="2" className="text-center"><h3>Loading data...</h3></td>
-                    </tr>
-                </tbody>
+                <tr>
+                    <td colSpan="9" className="text-center"><h3>Loading data...</h3></td>
+                </tr>
         }
         else {
 
             //const fdate = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).format(new Date(this.state.res.data.updated)) // 01/11/2021
-
+            var id = -1
             allData_html_table =
                 this.state.res.data.map((item) => {
+                    id = id + 1
                     return (
-                        <tr>
+                        <tr key={id}>
                             <td>{item.country}</td>
                             <td>{item.cases}</td>
                             <td>{item.todayCases}</td>
                             <td>{item.recovered}</td>
                             <td>{item.todayRecovered}</td>
+                            <td>{item.deaths}</td>
+                            <td>{item.todayDeaths}</td>
+                            <td>{item.tests}</td>
+                            <td>{item.updated}</td>
                         </tr>
                     )
+
                 })
         }
 
@@ -63,10 +67,22 @@ class CountryList extends Component {
                                 New Cases
                             </th>
                             <th>
-                                Total REcovered
+                                Total Recovered
                             </th>
                             <th>
                                 New Recovered
+                            </th>
+                            <th>
+                                Total Deaths
+                            </th>
+                            <th>
+                                New Deaths
+                            </th>
+                            <th>
+                                Total Tests
+                            </th>
+                            <th>
+                                Updated Date
                             </th>
                         </tr>
                     </thead>
